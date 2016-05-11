4ac1add72356f5fd9697d5aa111e975460e524e7
FoxDot <ryankirkbride26@gmail.com>
2016-05-11 09:54:44 +0100

    Version

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from distutils.core import setup
 
 setup(name='FoxDot',
+      version='0.1.2',
       description='Musical Live Coding Environment',
       author='Ryan Kirkbride',
       author_email='sc10rpk@leeds.ac.uk',
