b1dd6d16beaaac54058dd0d29c47ba3a3e03a6c0
FoxDot <ryankirkbride26@gmail.com>
2016-11-15 16:42:13 +0000

    added Bang.py

---
+++ FoxDot/Bang.py
@@ -0,0 +1,55 @@
+from Code import execute
+
+class Bang:
+
+    def __init__(self, player, kwargs):
+
+        self.widget = execute.namespace['FoxDot']
+
+        # Get visible portion of the text window
+
+        try:
+        
+            a = self.widget.text.index("@0,0")
+            b = self.widget.text.index("@0,%d" % self.widget.text.winfo_height())
+
+            a, b = (int(s.split(".")[0]) for s in (a, b))
+
+        except:
+
+            a, b = 9999, 0
+
+        # Only update visuals if the line is visible
+
+        if a <= player.line_number <= b:
+            
+            row = player.line_number
+            col = player.whitespace
+            env   = player.envelope
+            event = player.event
+            
+            duration = event['sus']
+
+            self.id = "{}_bang".format(player.id)
+
+            start = "%d.%d" % (row, col)
+            end   = "%d.end" % row
+
+            try:
+
+                self.widget.text.tag_add(self.id, start, end)
+                self.widget.text.tag_config(self.id, **kwargs)
+
+                self.duration = 0.1
+
+                player.metro.schedule(self.remove, player.metro.now() + self.duration)
+
+            except:
+
+                pass
+
+
+    def remove(self):
+        self.widget.text.tag_delete(self.id)
+        return
+      
