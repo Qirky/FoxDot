3a96a6d3fecdf300823a2f13fe89fa5e132fedfb
FoxDot <ryankirkbride26@gmail.com>
2017-11-09 12:00:56 +0000

    Update README

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup   
 
 setup(name='FoxDot',
-      version='0.4.13',
+      version='0.4.14',
       description='Live Coding with SuperCollider',
       author='Ryan Kirkbride',
       author_email='ryan@foxdot.org',
