0e6e7faa93ec368b04707415322fdc164822e3d5
FoxDot <ryankirkbride26@gmail.com>
2018-01-19 12:17:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='FoxDot',
-      version='0.5.6',
+      version='0.5.8',
       description='Live coding music with SuperCollider',
       author='Ryan Kirkbride',
       author_email='ryan@foxdot.org',
