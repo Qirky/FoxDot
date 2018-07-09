4abefe6761947ce00554ae6f602ffe0dc70a47e4
Ryan Kirkbride <ryankirkbride26@gmail.com>
2018-07-09 13:25:06 +0100

    Update version num in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
     long_description=f.read()
 
 setup(name='FoxDot',
-      version='0.6.6',
+      version='0.6.7',
       description='Live coding music with SuperCollider',
       author='Ryan Kirkbride',
       author_email='ryan@foxdot.org',
