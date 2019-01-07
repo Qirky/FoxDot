6414c3a82691d5aad647ca38e3b087256039e4ed
Ryan Kirkbride <ryankirkbride26@gmail.com>
2019-01-07 14:21:43 +0000

    Update TempoClock.py

--- FoxDot/lib/TempoClock.py
+++ FoxDot/lib/TempoClock.py
@@ -208,10 +208,9 @@ class TempoClock(object):
     def update_tempo(self, bpm):
         """ Schedules the bpm change at the next bar, returns the beat and start time of the next change """
         next_bar = self.next_bar()
-        bpm_start_time = time() + self.beat_dur(next_bar - self.now()) - self.nudge
+        bpm_start_time = time() + self.beat_dur(next_bar - self.now()) - (self.nudge + self.hard_nudge)
         bpm_start_beat = next_bar
         def func():
-            # print(time() - self.nudge, bpm_start_time, (time() - self.nudge) - bpm_start_time)
             object.__setattr__(self, "bpm", self._convert_json_bpm(bpm))
             self.last_now_call = self.bpm_start_time = bpm_start_time
             self.bpm_start_beat = bpm_start_beat
@@ -223,7 +222,6 @@ class TempoClock(object):
         """ Sets the bpm externally  from another connected instance of FoxDot """
         def func():
             object.__setattr__(self, "bpm", self._convert_json_bpm(bpm))
-            # print(time() - self.nudge, bpm_start_time, (time() - self.nudge) - bpm_start_time)
             self.last_now_call = self.bpm_start_time = bpm_start_time
             self.bpm_start_beat = bpm_start_beat
         # Give next bar value to bpm_start_beat
