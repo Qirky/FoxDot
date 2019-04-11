055e81262309e07be81620f605e4693fcc14b0f0
Ryan Kirkbride <ryankirkbride26@gmail.com>
2019-04-11 13:14:48 +0100

    Remove startup.py from gitignore

---
+++ FoxDot/lib/Custom/startup.py
@@ -0,0 +1,6 @@
+# startup.py
+# ----------
+# This file is loaded on startup of FoxDot. You can edit it to include any code
+# you want to run at the beginning of a session. To use an alternate startup
+# file use the --startup flag followed by a valid path. You can also run FoxDot
+# with a --no-startup flag to not load this file.
