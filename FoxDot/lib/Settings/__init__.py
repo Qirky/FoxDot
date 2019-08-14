6ba3618ad045ccd2e4c3a76873a05f9bfdf67e0b
Ryan Kirkbride <ryankirkbride26@gmail.com>
2019-08-14 13:02:57 +0100

    Add stretch to list of loop players

--- FoxDot/lib/Settings/__init__.py
+++ FoxDot/lib/Settings/__init__.py
@@ -124,14 +124,14 @@ def get_timestamp():
 # Name of SamplePlayer and LoopPlayer SynthDef
 
 class _SamplePlayer:
-    names = ('play1', 'play2')
+    names = ('play1', 'play2',)
     def __eq__(self, other):
         return other in self.names
     def __ne__(self, other):
         return other not in self.names
 
 class _LoopPlayer:
-    names = ("loop", "gsynth")
+    names = ("loop", "gsynth", 'stretch')
     def __eq__(self, other):
         return other in self.names
     def __ne__(self, other):
