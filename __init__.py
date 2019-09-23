4ede50ad5c6ba2bfc582495f5b31b0df7fe1017c
Moz <39520581+moz-ljp@users.noreply.github.com>
2019-09-23 20:19:35 +0100

    Added missing return False for linux os

--- __init__.py
+++ __init__.py
@@ -19,101 +19,96 @@ Copyright Ryan Kirkbride 2015
 
 from __future__ import absolute_import, division, print_function
 
-try:
+def boot_supercollider():
+    """ Uses subprocesses to boot supercollider from the cli """
+
+    import time
     import platform
-except:
-    print("Couldn't import platform library")
-
-import os
-
-import subprocess
-
-try:
-    import psutil
-except:
-    os.system("pip install psutil")
-
-try:
+    import os
+    import subprocess
     import getpass
-except:
-    print("Couldn't import getpass library")
+
+    try:
+        import psutil
+    except ImportError:
+        os.system("pip install psutil")
+        import sys
+        sys.exit("Installed psutil, please start FoxDot again.")
+
+    sclangpath = "" #find path to sclang
+
+    thispath = "" #find this path
+
+    thisdir = os.getcwd()
+
+    OS = platform.system()
+
+    username = getpass.getuser()
+
+    if(OS == "Windows"):
+
+        print("OS: Windows")
+
+        sclangloc = os.popen('where /R "C:\\Program Files" sclang.exe').read()
+
+        thiscwd = str(sclangloc)
+
+        ourcwd = thiscwd.replace('\\sclang.exe\n', '')
+
+        def is_proc_running(name):
+            for p in psutil.process_iter(attrs=["name", "exe", "cmdline"]):
+                #print(p);
+                procname = p.info['name'] or \
+                     p.info['exe'] and os.path.basename(p.info['exe']) == name or \
+                     p.info['cmdline'] and p.info['cmdline'][0] == name
+                if(procname.startswith(name)):
+                    return True
+            return False
 
 
-sclangpath = "" #find path to sclang
+        running = (is_proc_running("sclang"))
 
-thispath = "" #find this path
+        if(running == False):
+            startup = thisdir+"/FoxDot/startup.scd"
+            #os.system("sclang"+startup+" &")
+            subprocess.Popen([sclangloc, startup], cwd=ourcwd, shell=True)
 
-"""
+    elif(OS == "Linux"):
 
-Sclang path and supercollider path are the same but need to go to the actual file for sclang
+        print("OS: Linux")
 
-For CWR (current working directory) need to go to the folder that contains sclang (also contains supercollider (scide.exe))
-
-"""
-
-thisdir = os.getcwd()
-
-OS = platform.system()
-
-username = getpass.getuser()
-
-if(OS == "Windows"):
-
-    print("OS: Windows")
-
-    sclangloc = os.popen('where /R "C:\Program Files" sclang.exe').read()
-
-    thiscwd = str(sclangloc)
-
-    ourcwd = thiscwd.replace('\\sclang.exe\n', '')
-
-    def is_proc_running(name):
-        for p in psutil.process_iter(attrs=["name", "exe", "cmdline"]):
-            #print(p);
-            procname = p.info['name'] or \
-                 p.info['exe'] and os.path.basename(p.info['exe']) == name or \
-                 p.info['cmdline'] and p.info['cmdline'][0] == name
-            if(procname.startswith(name)):
-                return True
-        return False
+        def is_proc_running(name):
+            for p in psutil.process_iter(attrs=["name","cmdline"]):
+                #print(p);
+                procname = p.info['name'] or \
+                     p.info['cmdline'] and p.info['cmdline'][0] == name
+                if(procname.startswith(name)):
+                    return True
+            return False
 
 
-    running = (is_proc_running("sclang"))
+        running = (is_proc_running("sclang"))
 
-    if(running == False):
-        startup = thisdir+"/FoxDot/startup.scd"
-        #os.system("sclang"+startup+" &")
-        subprocess.Popen([sclangloc, startup], cwd=ourcwd, shell=True)
-
-elif(OS == "Linux"):
-
-    print("OS: Linux")
-
-    def is_proc_running(name):
-        for p in psutil.process_iter(attrs=["name","cmdline"]):
-            #print(p);
-            procname = p.info['name'] or \
-                 p.info['cmdline'] and p.info['cmdline'][0] == name
-            if(procname.startswith(name)):
-                return True
-        return False
+        if(running == False):
+            startup = thisdir+"/FoxDot/startup.scd"
+            #os.system('sclang "/home/foxdot/Desktop/FoxDot-Cross-Platform/FoxDot/startup.scd" &') #fuctional
+            os.system("sclang "+startup+" &")
 
 
-    running = (is_proc_running("sclang"))
+    else:
+        print("Operating system unrecognised")
+        #Potentially get the user to choose their OS from a list?
+        #Then run the corresponding functions
 
-    if(running == False):
-        startup = thisdir+"/FoxDot/startup.scd"
-        #os.system('sclang "/home/foxdot/Desktop/FoxDot-Cross-Platform/FoxDot/startup.scd" &') #fuctional
-        os.system("sclang "+startup+" &")
+import sys
 
+if "--boot" in sys.argv:
 
-else:
-    print("Operating system unrecognised")
-    #Potentially get the user to choose their OS from a list?
-    #Then run the corresponding functions
+    boot_supercollider()
+
+    sys.argv.remove("--boot")
 
 from .lib import *
-
 
 def main():
     """ Function for starting the GUI when importing the library """
