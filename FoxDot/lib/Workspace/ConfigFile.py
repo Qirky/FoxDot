810a7f6fbf0a1d882b26307b89b18777b318db35
Ryan Kirkbride <ryankirkbride26@gmail.com>
2019-09-24 11:05:53 +0100

    Update ConfigFile.py
    Improve usability of config file editor window

--- FoxDot/lib/Workspace/ConfigFile.py
+++ FoxDot/lib/Workspace/ConfigFile.py
@@ -16,6 +16,7 @@ class Config:
     def __init__(self, path):
         self.root = Toplevel()
         self.root.title("conf.txt")
+        self.root.protocol("WM_DELETE_WINDOW", self.save_and_close )
 
         try:
 
@@ -34,8 +35,9 @@ class Config:
 
         self.textbox = Text(self.root, width=50, yscrollcommand=self.y_scroll.set)
         self.textbox.grid(row=0, column=0, columnspan=2)
+        self.y_scroll.config(command=self.textbox.yview)
 
-        self.exit = Button(self.root, text="Cancel", command=self.root.destroy)
+        self.exit = Button(self.root, text="Cancel", command=self.save_and_close)
         self.exit.grid(row=1, column=0, stick=N+S+E+W)
 
         self.save = Button(self.root, text="Save Changes", command=self.save_changes)
@@ -45,15 +47,30 @@ class Config:
 
         with open(self.filepath) as f:
 
-            self.text = f.read()
+            self.text = f.read().rstrip()
 
         self.textbox.insert(INSERT, self.text)
+
+        # Add binds?
+
+        # self.textbox.bind()
         
     def start(self):
         self.root.mainloop()
 
+    def save_and_close(self, event=None):
+        """ Asks the user if they want to save changes """
+        if self.get_text() != self.text:
+            answer = tkMessageBox.askyesno("Save changes", "Do you want to save your changes?")
+            if answer:
+                return self.save_changes()
+        return self.root.destroy()
+
+    def get_text(self):
+        return self.textbox.get("0.0", END).rstrip()
+
     def save_changes(self):
-        text = self.textbox.get("0.0", END)
+        text = self.get_text()
         f = open(self.filepath, "w") # writing a file
         f.write(text)
         f.close()
