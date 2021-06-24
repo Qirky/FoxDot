ee48471578e2d02d40ae46752107825494da6865
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2021-06-24 10:21:00 -0500

    Delete info file only when template file exists

--- FoxDot/lib/Settings/__init__.py
+++ FoxDot/lib/Settings/__init__.py
@@ -123,11 +123,12 @@ if conf.SAMPLES_DIR is not None and conf.SAMPLES_DIR != "":
     FOXDOT_SND = os.path.realpath(conf.SAMPLES_DIR)
 
 # Recreate info file from template
-if os.path.isfile(FOXDOT_INFO_FILE):
-    os.remove(FOXDOT_INFO_FILE)
-
 try:
     with open(FOXDOT_INFO_FILE+".template", "r") as fread:
+        # Delete info file only if template file exists
+        if os.path.isfile(FOXDOT_INFO_FILE):
+            os.remove(FOXDOT_INFO_FILE)
+
         content = fread.read()
         content = content.replace("<FOXDOT_ROOT>", f"'{FOXDOT_ROOT}'")
         content = content.replace("<FOXDOT_SND>", f"'{FOXDOT_SND}'")
