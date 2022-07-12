4305ee4cb8fc5ca59bb2bb7ac5975ac7459018d0
Ethen Dowdall <19dowdalle@utcsheffield.org.uk>
2022-07-12 12:07:56 +0100

    add PBase(n, b=2, l=1): Returns the 'n' number in base 'b' split into digits.  e.g. `PBase(5)` will return `P[1,0,1]`  and  `PBase(5,4)` will return `P[1,1]`  and  `PBase(5,4,4)` will return `P[0,0,1,1]`

--- FoxDot/lib/Patterns/Sequences.py
+++ FoxDot/lib/Patterns/Sequences.py
@@ -315,6 +315,32 @@ def PStrum(n=4):
     """ Returns a pattern of durations similar to how you might strum a guitar """
     return (Pattern([1,1/2]).stutter([1,n + 1])|Pattern([1.5,1/2]).stutter([1,n])|1)
 
+@loop_pattern_func
+def PBase(n, b=2, l=1):
+    ''' Returns the 'n' number in base 'b' split into digits.
+        e.g. `PBase(5)` will return `P[1,0,1]`
+        and  `PBase(5,4)` will return `P[1,1]`
+        and  `PBase(5,4,4)` will return `P[0,0,1,1]`
+    '''
+    number = 0+n
+
+    from_base10_to_anybase_num = [] # Initialize the number in any base
+    while number > 0: # Iterate while the number is greater than zero
+        remainder = int(number % b) # change remainder in integer
+        #from_base10_to_anybase_num.append( remainder )
+        from_base10_to_anybase_num.insert(0, remainder )
+        number //= b # take the integer part after division
+    
+    number_list = [int(i) for i in from_base10_to_anybase_num]
+    
+    while(len(number_list) < l):
+        number_list.insert(0, 0)
+    
+    #print("from_10_to_anybase("+str(num)+","+str(base)+")", number_list)
+    
+    return Pattern( number_list )
+
+
 def PQuicken(dur=1/2, stepsize=3, steps=6):
     """ Returns a PGroup of delay amounts that gradually decrease """
     delay = []
